310d267b59aaab84868b3b8c7776edf51237ba1e
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2024-01-20 14:39:49 +0700

    add create, update, delete product

--- main.go
+++ main.go
@@ -34,18 +34,34 @@ func sayHello(w http.ResponseWriter, r *http.Request)  {
 
 // get all data products
 func productsController(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type", "application/json")
-	result , _ := json.Marshal(products)
-	w.WriteHeader(http.StatusOK)
-	w.Write(result)
+	if r.Method == "GET"{
+		w.Header().Set("Content-Type", "application/json")
+		result , _ := json.Marshal(products)
+		w.WriteHeader(http.StatusOK)
+		w.Write(result)
+	} else if  r.Method == "POST"{
+		var product product
+		err := json.NewDecoder(r.Body).Decode(&product)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintln(w, "Invalid reqeust body")
+			return
+		}
+		products = append(products, product)
+		w.WriteHeader(http.StatusCreated)
+		fmt.Fprintln(w, "Product Created")
+		return
+	}
+	http.Error(w, "", http.StatusBadRequest)
 }
 
-// get data products by id
+// get, create, update, delete data products by id
 func productController(w http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-Type", "application/json")
 	idParam := r.URL.Path[len("/product/"):]
 	id, _ := strconv.Atoi(idParam)
-	
+
+	// Check id
 	var foundIndex = -1
 	for i, p := range products{
 		if p.Id == id{
@@ -53,13 +69,33 @@ func productController(w http.ResponseWriter, r *http.Request)  {
 			break
 		}
 	}
-
 	if foundIndex == -1 {
 		http.Error(w, "Product Not Found", http.StatusNotFound)
 		return
 	}
 
-	result , _ := json.Marshal(products[foundIndex])
-	w.WriteHeader(http.StatusOK)
-	w.Write(result)
+	if r.Method == "GET" {	
+		result , _ := json.Marshal(products[foundIndex])
+		w.WriteHeader(http.StatusOK)
+		w.Write(result)
+	} else if r.Method == "PUT" {
+		var updateProduct product
+
+		err := json.NewDecoder(r.Body).Decode(&updateProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintln(w, "Invalid request body")
+			return
+		}
+
+		products[foundIndex] = updateProduct
+		w.WriteHeader(http.StatusAccepted)
+		fmt.Fprintln(w, "Product Updated")
+		return
+	} else if r.Method == "DELETE" {
+		_ = append(products[:foundIndex], products[foundIndex+1:]...)
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "Product Deleted")
+	}
+	http.Error(w, "", http.StatusBadRequest)
 }
